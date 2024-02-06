75d2342b0650fc0f49db495099e2e922ee81730a
necipsagiro <sagiroglun22@itu.edu.tr>
2024-02-06 13:06:57 +0300

    Update file paths and remove unnecessary code

--- app/src/js/functions/renderPugFile.js
+++ app/src/js/functions/renderPugFile.js
@@ -2,16 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const pug = require('pug');
 
-const tempFilePath = path.join(__dirname, '../../../../resources/render/temp.html');
+const tempFilePath = path.join(__dirname, '../../../../resources/temp.html');
 
 module.exports = (pugFile, locals, callback) => {
   const htmlContent = pug.renderFile(
     path.join(__dirname, `../../views/${pugFile}.pug`),
     locals
   );
-
-  if (!fs.existsSync(tempFilePath))
-    fs.mkdirSync(path.dirname(tempFilePath), { recursive: true });
 
   fs.writeFile(tempFilePath, htmlContent, err => {
     if (err)
