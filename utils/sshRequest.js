bbe7ebfb76b4e5c9ea743ae72013d9955e7c6e62
YakupAltay <yakup.altay@boun.edu.tr>
2024-02-09 20:35:59 +0300

    Refactor sshRequest.js: Add IsSSHKeyEncrypted and getAuthMethod functions

--- utils/sshRequest.js
+++ utils/sshRequest.js
@@ -17,9 +17,34 @@ const conn = new ssh2.Client();
 // };
 
 
-// IsSSHKeyEncrypted() here
+const IsSSHKeyEncrypted = (privateKey) => {
+  const parsedKey = utils.parseKey(privateKey);
 
-// getAuthMethod() here
+  if (Array.isArray(parsedKey)) {
+    return parsedKey.some(key =>
+      key instanceof Error &&
+      key.message.includes('Encrypted private key detected, but no passphrase given')
+    );
+  } else if (parsedKey instanceof Error) {
+    return parsedKey.message.includes('Encrypted private key detected, but no passphrase given');
+  } else {
+    return false;
+  };
+};
+
+const getAuthMethod = (data) => {
+  if (data.password) {
+    return { password: data.password };
+  } else if (privateKeyPath) {
+    const privateKey = readFileSync(privateKeyPath, 'utf8');
+    const auth = { privateKey: Buffer.from(privateKey) };
+
+    if (IsSSHKeyEncrypted(privateKey))
+      auth.passphrase = data.passphrase;
+
+    return auth;
+  };
+};
 
 module.exports = (type, data, callback) => {
   if (!type || typeof type != 'string' || !TYPE_VALUES.includes(type))
@@ -29,13 +54,33 @@ module.exports = (type, data, callback) => {
     return callback('bad_request');
 
   if (type == 'connect') {
-    // type checks here
-
-    // connection here
+    conn.on('ready', () => {
+      callback(null, 'Connected');
+    }).on('error', err => {
+      callback(err);
+    }).connect({
+      host: data.host,
+      port: data.port,
+      username: data.username,
+      ...getAuthMethod(data)
+    });
+  } else if (type == 'disconnect') {
+    conn.end();
+    callback();
   } else if (type == 'exec') {
-    // type checks here
+    conn.exec(data.command, (err, stream) => {
+      if (err) return callback(err);
 
-    // exec here
+      let stdout = '';
+
+      stream.on('data', data => {
+        stdout += data;
+      });
+      
+      stream.on('close', () => {
+        callback(null, stdout);
+      });
+    });
   } else if (type == 'stream') {
     // leave this for now
   };
