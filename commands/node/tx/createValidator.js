87794dcc55b9d8764bd03ad0932b52402d8540f7
necipsagiro <sagiroglun22@itu.edu.tr>
2024-06-20 18:16:35 +0300

    feat: add notifications

--- commands/node/tx/createValidator.js
+++ commands/node/tx/createValidator.js
@@ -1,2 +1,11 @@
-module.exports = _ => `
+const createGasFeeFlags = require('./functions/createGasFeeFlags');
+
+module.exports = (from_key_name, fees) => `
+  $DAEMON_NAME tx staking create-validator \\
+    --chain-id $CHAIN_ID \\
+    --keyring-backend test \\
+    --yes \\
+    --output json \\
+    ${createGasFeeFlags(fees)} \\
+    --from ${from_key_name}
 `;
--- commands/node/tx/voteProposal.js
+++ commands/node/tx/voteProposal.js
@@ -3,6 +3,7 @@ const createGasFeeFlags = require('./functions/createGasFeeFlags');
 module.exports = (from_key_name, proposal_id, option, fees) => `
   $DAEMON_NAME tx gov vote ${proposal_id} ${option} \\
     --chain-id $CHAIN_ID \\
+    --keyring-backend test \\
     --yes \\
     --output json \\
     ${createGasFeeFlags(fees)} \\
