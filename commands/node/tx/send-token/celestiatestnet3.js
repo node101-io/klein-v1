f95685b80315a789c84cd7ad5bc8b86584144e28
necipsagiro <sagiroglun22@itu.edu.tr>
2024-07-01 16:52:33 +0300

    refactor: Update send-token command to handle non-generic transactions

--- commands/node/tx/send-token/celestiatestnet3.js
+++ commands/node/tx/send-token/celestiatestnet3.js
@@ -1,11 +1,10 @@
-const createGasFeeFlags = require('../functions/createGasFeeFlags');
-
 module.exports = data => `
   $DAEMON_NAME tx bank send ${data.from_key_name.trim()} ${data.to_address.trim()} ${data.amount}$DENOM \\
     --chain-id $CHAIN_ID \\
     --keyring-backend test \\
     --yes \\
     --output json \\
-    ${createGasFeeFlags(data.fees)} \\
+    --fees ${data.fees} \\
+    --gas 300000 \\
     --from ${data.from_key_name.trim()}
 `;
